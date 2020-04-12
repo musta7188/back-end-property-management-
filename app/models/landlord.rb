05a2dfd59d417fd3aca57acac48a81c7107aa33c
alexandre jamarco <alexandre.j@gmail.com>
2020-04-12 01:33:18 +0100

    added functionality

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -13,9 +13,4 @@ validates :email, presence: true
 validates :email, uniqueness: true
 validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> show-properties
 end
