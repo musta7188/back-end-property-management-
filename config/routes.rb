cc7b9bcc7cbf1cdb72226aaaeb16c9e10810ceb7
alexandre jamarco <alexandre.j@gmail.com>
2020-04-17 13:06:59 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,5 @@ Rails.application.routes.draw do
 
   resources  :landlords
   resources :properties
-<<<<<<< HEAD
   resources :tenants
-=======
-  resources :issues
->>>>>>> e84fba8e9a211e22487c9dad8833ff27932d5f0f
 end
